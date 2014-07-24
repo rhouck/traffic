24b510d30c5a70bf1ba7d8bafe865339c267f99f
rhouck <ryanchouck@gmail.com>
2014-07-23 18:45:31 -0700

    removed live debug mode

--- traffic/settings_live.py
+++ traffic/settings_live.py
@@ -8,9 +8,9 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
